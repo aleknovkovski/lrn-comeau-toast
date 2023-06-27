2ca72a753c3fb8401e9a4937af826d14da2c7b45
Alek Novkovski <alek@greatcode2.com>
2023-06-27 18:46:41 +0200

    Implement Toasts array and map over in ToastShelf

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -4,14 +4,16 @@ import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
 function ToastShelf() {
+    const [Toasts, setToasts] = React.useState([{message: 'Example notice toast', variant: 'notice'}, {message: 'Example error toast', variant: 'error'}]);
+
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+        {Toasts.map((item) => (
+            <Toast variant={item.variant}
+                   handleToastClose={() => setToasts(Toasts.filter((toast) => toast !== item))}>
+                {item.message}
+            </Toast>
+        ))}
     </ol>
   );
 }
